bfcd57fa56fa20a90756f7e83b2f1c821d826337
zednegr <negrzed@gmail.com>
2023-08-18 12:56:59 +0500

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,33 @@
 import "./header.scss"
+import 'boxicons'
 
 function Header() {
 
     return (
-        <section>
+        <section className="navbar-section">
             <div className="container">
-                <div className="header-wrapper">
-                    <h1>HI</h1>
-                </div>
+                <nav className="navbar">
+
+                    <div className="navbar-left">
+
+                        <div className="navbar-brand">
+                            <h2 className="brand-h2">QPICK</h2>
+                        </div>
+
+                        <div className="navbar-filter">
+                            <box-icon type='solid' name='mobile'></box-icon>
+                            <h4 className="navbar-filter-h4">Выбрать модель телефона</h4>
+                            <select name="" id=""></select>
+                        </div>
+
+                    </div>
+
+                    <ul className="nav-list">
+                        <li><box-icon name='heart'></box-icon></li>
+                        <li><box-icon name='shopping-bag'></box-icon></li>
+                    </ul>
+
+                </nav>
             </div>
         </section>
     )
