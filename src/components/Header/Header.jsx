5c73af69877944293868c107edf763f71fbc751d
zednegr <negrzed@gmail.com>
2023-08-18 12:29:50 +0500

    update

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./header.scss"
+
+function Header() {
+
+    return (
+        <section>
+            <div className="container">
+                <div className="header-wrapper">
+                    <h1>HI</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Header
