9d1e82cdd343fa188bcebb70af7a2edc2d7d46c9
zednegr <negrzed@gmail.com>
2023-08-18 13:51:28 +0500

    update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./footer.scss"
+
+function Footer() {
+
+    return (
+        <section>
+            <div className="container">
+                <div className="footer-wrapper">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
