dbfc32eaf567244a8262c6e1c2c018d6705993e8
zednegr <negrzed@gmail.com>
2023-08-20 10:58:31 +0500

    update

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,18 @@
+import './loading.scss'
+
+function Loading() {
+    
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <div className="loading">
+                    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Loading
