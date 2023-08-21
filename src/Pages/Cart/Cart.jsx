c4e7a54b024a12a998e02b03aba563ea26b13288
zednegr <negrzed@gmail.com>
2023-08-21 10:48:22 +0500

    update

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -20,23 +20,10 @@ function Cart() {
     const [loading, setLoading] = useState(true);
     const { productId } = useParams();
 
-    // Counter
-    const [count, setCount] = useState(1);
-
 
     const [productPrice, setProductPrice] = useState();
     const [price, setPrice] = useState(setProductPrice);
     const [delivery, setDelivery] = useState(499)
-
-    const plus = () => {
-        setCount(count + 1);
-    };
-
-    const minus = () => {
-        if (count > 1) {
-            setCount(count - 1);
-        }
-    };
 
     useEffect(() => {
         axios('https://64e080ca50713530432c5e47.mockapi.io/apple-shop/apple-cart')
@@ -56,75 +43,47 @@ function Cart() {
     return (
         <section className="cart-section">
             <div className="conatiner">
-                <div className="addToCart-wrapper">
-                    <div className="addToCart-card-map">
+                <div className="cart-wrapper">
 
-                        {
-                            getData?.map((item, index) => {
-                                console.log(item.getData.name);
-                                return (
+                    {
+                        getData?.map((item, index) => {
+                            console.log(item.getData.name);
+                            return (
 
-                                    <>
-                                        <div className="addToCart-card" key={item.getData.id}>
-                                            <div className="addToCart-card-top">
+                                <div className="addToCart-card" key={item.getData.id}>
+                                    <div className="addToCart-card-top">
 
-                                                <NavLink to={`/cards_inner/${item.id}`}>
-                                                    <div className="addToCart-product">
-                                                        <img
-                                                            className="addToCart-product-img"
-                                                            width={"146px"}
-                                                            height={"136px"}
-                                                            src={item?.getData?.img}
-                                                        />
-                                                    </div>
-                                                </NavLink>
-
-                                                <NavLink to={`/cards_inner/${item.id}`}>
-                                                    <div className="addToCart-product-name">
-                                                        <h4 className="addToCart-product-name-h4">{item?.getData?.name}</h4>
-                                                        <h4 className="addToCart-product-price-h4">{item?.getData.price} ₸</h4>
-                                                    </div>
-                                                </NavLink>
+                                        <NavLink to={`/cards_inner/${item.id}`}>
+                                            <div className="addToCart-product">
+                                                <img
+                                                    className="addToCart-product-img"
+                                                    width={"146px"}
+                                                    height={"136px"}
+                                                    src={item?.getData?.img}
+                                                />
                                             </div>
+                                        </NavLink>
 
-                                            <div className="addToCart-middle">
-                                                <div className="addToCart-middle-counter">
-                                                    <button
-                                                        className="addToCart-middle-counter-minus-btn"
-                                                        onClick={minus}
-                                                    >
-                                                        -
-                                                    </button>
-                                                    <h4 className="addToCart-middle-counter-h4">{count}</h4>
-                                                    <button
-                                                        className="addToCart-middle-counter-plus-btn"
-                                                        onClick={plus}
-                                                    >
-                                                        +
-                                                    </button>
-                                                </div>
-
-                                                <div className="addToCart-middle-price">
-                                                    <h4 className="addToCart-middle-price-h4">
-                                                        {item?.getData.price}₸
-                                                    </h4>
-                                                </div>
+                                        <NavLink to={`/cards_inner/${item.id}`}>
+                                            <div className="addToCart-product-name">
+                                                <h4 className="addToCart-product-name-h4">{item?.getData?.name}</h4>
+                                                <h4 className="addToCart-product-price-h4">{item?.getData.price} ₸</h4>
                                             </div>
+                                        </NavLink>
+                                    </div>
+
+                                    <div className="addToCart-middle">
+                                        <div className="addToCart-middle-price">
+                                            <h4 className="addToCart-middle-price-h4">
+                                                {item?.getData.price}₸
+                                            </h4>
                                         </div>
+                                    </div>
+                                </div>
+                            )
 
-                                    </>
-
-
-                                )
-
-                            })
-                        }
-
-
-
-
-                    </div>
-
+                        })
+                    }
 
 
                 </div>
